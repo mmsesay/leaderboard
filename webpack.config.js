57691fc3f7440cc710cb38f3fb19285676e29a34
mmsesay <maej_01@hotmail.com>
2021-10-07 13:49:52 +0000

    add title bar svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -17,6 +18,8 @@ module.exports = {
   output: {
     filename: 'bundel.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+    assetModuleFilename: '[name][ext]',
     clean: true,
   },
   module: {
@@ -25,6 +28,29 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets/',
+          publicPath: 'assets/',
+          emitFile: true,
+          // esModule: false,
+        },
+      },
     ],
   },
 };
+
+// {
+//   loader: 'file-loader',
+//   options: {
+//     name: '[name].[ext]',
+//     outputPath: 'dist/assets/',
+//   },
+// },
